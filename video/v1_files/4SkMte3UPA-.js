917857c65ec8523a437313ded627daa4754005be
ionnbote <iondbotezatu@yahoo.com>
2025-05-03 10:19:32 +0300

    Removed unused folder

--- video/v1_files/4SkMte3UPA-.js
+++
@@ -1,4 +0,0 @@
-;/*FB_PKG_DELIM*/
-
-__d("FDSFollowFilled20PNGIcon.react",["ix","fbicon"],(function(a,b,c,d,e,f,g,h){"use strict";a=d("fbicon")._(h("618353"),20);b=a;g["default"]=b}),98);
-__d("FDSFriendConfirmFilled16PNGIcon.react",["ix","fbicon"],(function(a,b,c,d,e,f,g,h){"use strict";a=d("fbicon")._(h("538341"),16);b=a;g["default"]=b}),98);
--- video/v1_files/YiqQZv3pBtv.js
+++
@@ -1,4 +0,0 @@
-;/*FB_PKG_DELIM*/
-
-__d("FDSRefreshRightFilled16Icon",["cr:15576"],(function(a,b,c,d,e,f,g){"use strict";g["default"]=b("cr:15576")}),98);
-__d("FDSRefreshRightFilled16PNGIcon.react",["ix","fbicon"],(function(a,b,c,d,e,f,g,h){"use strict";a=d("fbicon")._(h("580897"),16);b=a;g["default"]=b}),98);
